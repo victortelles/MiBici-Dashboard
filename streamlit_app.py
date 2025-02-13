d90d9434f4e9240241fe11f6fe1f943519ca568b
victortelles <victor.telles@iteso.mx>
2025-02-12 23:54:24 -0600

    Intento de lectura de datos | Falla por caracteres especiales

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,49 @@ import seaborn as sns
 import streamlit as st
 import random
 from skimage import io
+import os
+
+#----- Lectura de datos -----------------------------------
+#Ruta de los datos de MiBici
+data_folder = r'data\MiBici-Data'
+#print(f'Ruta de la carpeta: {data_folder}')
+
+#Almacenar datos
+csv_files = []
+
+# Funcion para recorrer todos los archivos y subcarpetas
+for root, dirs, files in os.walk(data_folder):
+    for file in files:
+        # Lectura de archivo CSV
+        if file.endswith(".csv"):
+            csv_files.append(os.path.join(root, file))
+
+# verificar si se encuentran los archivos/datos
+if not csv_files:
+    st.error('No se encontraron los archivos CSV en la carpeta especificada.')
+
+else:
+    st.success(f'se encontraron {len(csv_files)} archivos CSV')
+    st.write(csv_files)
+
+if csv_files:
+    #Guardar los datos en el d.f
+    dataframes = []
+    for file in csv_files:
+        try:
+            df = pd.read_csv(file)
+            dataframes.append(df)
+        except Exception as e:
+            st.error(f'Error al leer el archivo {file}: {e}')
+
+    # Verificar el dataframe si hay datos
+    if dataframes:
+        df = pd.concat(dataframes, ignore_index=True)
+        st.write('Datos cargados correctamente:')
+        st.write(df.head(100))
+    else:
+        st.error('No se encontraron datos en los archivos CSV')
+
 
 #------------------------------------------------------------------
 #----- Configuración Sidebar --------------------------------------
@@ -48,10 +91,9 @@ st.subheader(":blue[Streamlit es un *framework* para la creación de aplicacione
 
 #----- Renderizado de la Imagen y el Título en el Dashboard -------
 st.sidebar.image(Logo, width = 200)
-st.sidebar.markdown("## MENÚ DE CONFIGURACIÓN")
+st.sidebar.markdown("## MENU DE FILTROS")
 st.sidebar.divider()
 
-#----- HISTOGRAMA POR MES -----------------------------------------
 #----- Selector del Mes -------------------------------------------
 vars_mes = ['ENE','FEB','MAR','ABR','MAY','JUN','JUL','AGO','SEP','OCT','NOV','DIC']
 default_hist = vars_mes.index('ENE')
